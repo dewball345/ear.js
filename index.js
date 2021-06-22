66dd51b0b78ba048c614f892cfa83bcdd06bfe14
abd <abhiraamkumar@gmail.com>
2021-06-22 13:02:53 -0700

    fixed nodetection bug

--- index.js
+++ index.js
@@ -4,20 +4,24 @@ console.log("ENTERED")
 var canvas = document.getElementById("canvas")
 var ctx = canvas.getContext("2d");
 var img = document.getElementById("img");
+var status = document.getElementById("status");
 
 async function main(){
     let im0s = tf.browser.fromPixels(img)
     await tf.browser.toPixels(im0s, canvas);
 
     let em = new EarModel()
+    status.innerText = "Loading model"
     await em.loadModel()
 
     let pred;
+    status.innerText = "Starting inference"
     tf.engine().startScope()
     pred = await em.pred(canvas, ctx, img);
     tf.engine().endScope()
 
     // console.log(fpscheck + " FPS")
+    status.innerText = "Image is drawn to canvas!"
     pred.forEach(det => {
         GeneralUtils.drawToCanvas(det, ctx)
     })
